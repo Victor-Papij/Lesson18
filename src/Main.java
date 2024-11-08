8ca76206b9432063a6be410a58e1c67b235ed1d9
Victor-Papij <papijviktor@gmail.com>
2024-11-08 17:38:34 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Task1
+        LocalDate date = LocalDate.of( 2024, Month.JANUARY, 2);
+        System.out.println(date);
+        //Task2
+        LocalTime time = LocalTime.of( 13,20);
+        System.out.println(time);
+        //Task3
+        LocalDateTime dateTime = LocalDateTime.of(date,time);
+        System.out.println(dateTime);
+        //Task4
+        ZonedDateTime zonedDateTime1 = dateTime.atZone(ZoneId.of("America/New_York"));
+        System.out.println(zonedDateTime1);
+        //Task5
+        LocalDateTime dateTime2 = dateTime
+                .plusMinutes(1)
+                .plusHours(1)
+                .plusDays(1)
+                .plusMonths(1)
+                .plusYears(1);
+        System.out.println(dateTime2);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
